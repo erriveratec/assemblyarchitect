2785437c1dfa6c1f43fa79dbf5d2ef613fd3b4bf
Ernesto Rivera <ernestoriv7@yahoo.com>
2026-07-10 18:35:51 -0600

    Push before menu refactor

--- nvimscript.sh
+++ nvimscript.sh
@@ -4,6 +4,7 @@ tabnew source/stages.c | vsplit source/stages.h |
 tabnew source/game_mechanics_mc.c | vsplit source/game_mechanics_mc.h | 
 tabnew source/levels_lv.c | vsplit source/levels_lv.h | 
 tabnew source/stage_buttons_sb.c | vsplit source/stage_buttons_sb.h |
+tabnew source/escape_menu_em.c | vsplit source/escape_menu_em.h |
 tabnew source/registers_rg.c | vsplit source/registers_rg.h |
 tabnew source/buffers_bf.c | vsplit source/buffers_bf.h |
 tabnew source/instruction_window_iw.c | vsplit source/instruction_window_iw.h |
@@ -11,7 +12,7 @@ tabnew source/code_line_cl.c | vsplit source/code_line_cl.h |
 tabnew source/code_window_cw.c | vsplit source/code_window_cw.h | 
 tabnew source/button_bt.c | vsplit source/button_bt.h | 
 tabnew source/arrow_ar.c | vsplit source/arrow_ar.h | 
-tabnew source/text_tx.c | vsplit source/text_tx.h | 
+tabnew source/text_tx.c | vsplit source/text_tx.j | 
 tabnew source/aux.c| vsplit source/aux.h | 
 tabnew source/list.c | vsplit source/list.h | 
 tabnew source/file_fl.c | vsplit source/file_fl.h |
